31a5bc84ab1f7b93d9e7c77ab106058dc2034f5e
wenxy <wenxianyiwork@163.com>
2014-08-12 23:09:06 +0800

    初始化仓库

---
+++ sorter/src/algorithms/sorter.go
@@ -0,0 +1,100 @@
+/* 主程序主要完成以下工作：
+ * 	a) 获取并解析命令行输入；
+ *  b）从对应文件中读取输入数据；
+ *  c) 调用对应的排序数据；
+ *  d) 将排序的结果输出到对应的文件中
+ *  e) 打印排序所花费的时间
+ */
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"time"
+)
+
+var infile *string = flag.String("i", "infile", "File contains values for sotrting.")
+var outfile *string = flag.String("o", "outfile", "File to receive sorted values.")
+var algorithm *string = flag.String("a", "qsort", "Sort algorithm.")
+
+func readValues(infile string) (values []int, err error) {
+	file, err := os.Open(infile)
+	if err != nil {
+		fmt.Println("Failed to open the input file ", infile)
+		return
+	}
+	defer file.Close()
+
+	br := bufio.NewReader(file)
+	values = make([]int, 0)
+	for {
+		line, isPrefix, err1 := br.ReadLine()
+		if err1 != nil {
+			if err1 != io.EOF {
+				err = err1
+			}
+			break
+		}
+		if isPrefix {
+			fmt.Println("A too long line, seems unexpected.")
+			return
+		}
+
+		str := string(line) // 转换字符数组为字符串
+
+		value, err1 := strconv.Atoi(str)
+
+		if err1 != nil {
+			err = err1
+			return
+		}
+
+		values = append(values, value)
+	}
+	return
+}
+
+func writeValues(values []int, outfile string) error {
+	file, err := os.Create(outfile)
+	if err != nil {
+		fmt.Println("Failed to create the output file ", outfile)
+		return err
+	}
+
+	defer file.Close()
+
+	for _, value := range values {
+		str := strconv.Itoa(value)
+		file.WriteString(str + "\n")
+	}
+	return nil
+}
+func main() {
+	flag.Parse()
+	if infile != nil {
+		fmt.Println("infile = ", *infile, "outfile = ", *outfile, "algorithm = ", *algorithm)
+	}
+	values, err := readValues(*infile)
+	if err == nil {
+		t1 := time.Now()
+		switch *algorithm {
+		/* case "qsort":
+		       qsort.QuickSort(values)
+		   case "bubblesort":
+		       bubblesort.BubbleSort(values) */
+		default:
+			fmt.Println("Sorting algorithm", *algorithm, "is either unknown or unsupported.")
+		}
+		t2 := time.Now()
+
+		fmt.Println("The sorting process costs", t2.Sub(t1), "to complete.")
+
+		writeValues(values, *outfile)
+	} else {
+		fmt.Println(err)
+	}
+}
